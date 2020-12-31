354a3713d35eda79bf0c392ee037e3f4187f1f28
TheMasteredPanda <dukejakemorgan@gmail.com>
2020-12-31 19:58:24 +0000

    Update with comment code.

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 document.getElementsByName("inputForm")[0].addEventListener("submit", (e) => {
   let element = document.getElementsByName("inputText")[0];
   e.preventDefault();
+  //By not escaping the special characters that make up the xms tags the js code within
+  //The tags will be embedded into the web page, executing the code. - Duke J. Morgan
   let output = document.getElementsByClassName("output")[0];
   output.innerHTML = element.value;
 });
